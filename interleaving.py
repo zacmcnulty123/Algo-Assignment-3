1da4f384c15138e2bee820e44cbe729328728f15
Zachary McNulty <zacharymcnulty@Zacharys-MBP.fios-router.home>
2020-04-28 18:14:35 -0400

    Added some trace run logic

--- interleaving.py
+++ interleaving.py
@@ -1,60 +1,44 @@
- 
+import argparse
+import sys
+
+numComparisions = 0
 def isInterleaved(X, Y, S):
-    A = X  
+    #Need at least 2 of X and Y for this to be true
+    global numComparisions
+    if len(S) < 2*(len(X) + len(Y)):
+      return False
+    A = X
     B = Y
     while len(A) < len(S):
       A = A + X
     while len(B) < len(S):
       B = B + Y
-    # Find lengths of the two strings 
-    M = len(A) 
-    N = len(B)  
+    M = len(A)
+    N = len(B)
     opt = [[False] * (N+1) for i in range(M+1)]
-    # C can be an interleaving of A and B only of sum  
-    # of lengths of A & B is equal to length of C.  
-    # if ((M + N) > len(C)):  
-    #     return False
-    # Process all characters of A and B 
-    print(A)
-    print(B)
-    print(S)
-    for i in range(0, M+1):  
-        for j in range(0, N+1): 
+    for i in range(0, M+1):
+        numComparisions = numComparisions + 1
+        for j in range(0, N+1):
+            numComparisions = numComparisions + 1
             if ((i + j - 1) > len(S)-1):
               break
-            # two empty strings have an empty string  
-            # as interleaving  
-            if (i == 0 and j == 0):  
-                opt[i][j] = True
-  
+            if (i == 0 and j == 0):
+              opt[i][j] = True
             # A is empty  
-            elif (i == 0):  
-                if (B[j - 1] == S[j - 1]):  
-                    opt[i][j] = opt[i][j - 1]  
-              
+            elif (i == 0):
+              if (B[j - 1] == S[j - 1]):
+                opt[i][j] = opt[i][j - 1]
             # B is empty  
             elif (j == 0):
-                if (A[i - 1] == S[i - 1]):  
-                    opt[i][j] = opt[i - 1][j]  
-              
-            # Current character of C matches with  
-            # current character of A, but doesn't match  
-            # with current character of B  
+              if (A[i - 1] == S[i - 1]):
+                opt[i][j] = opt[i - 1][j]
+            elif (A[i - 1] == S[i + j - 1] and B[j - 1] != S[i + j - 1]):
+              opt[i][j] = opt[i - 1][j]
+            elif (A[i - 1] != S[i + j - 1] and B[j - 1] == S[i + j - 1]):
+              opt[i][j] = opt[i][j - 1]
             elif (A[i - 1] == S[i + j - 1] and
-                  B[j - 1] != S[i + j - 1]):  
-                opt[i][j] = opt[i - 1][j]  
-  
-            elif (A[i - 1] != S[i + j - 1] and 
-                  B[j - 1] == S[i + j - 1]):  
-                opt[i][j] = opt[i][j - 1]  
-  
-            elif (A[i - 1] == S[i + j - 1] and 
-                  B[j - 1] == S[i + j - 1]):  
-                opt[i][j] = (opt[i - 1][j] or opt[i][j - 1])  
-    for i in range(0, M+1):
-      for j in range(0, N+1):
-        print(str(opt[i][j]) + " "),
-      print("\n")
+              B[j - 1] == S[i + j - 1]):
+              opt[i][j] = (opt[i - 1][j] or opt[i][j - 1])
     repeatX = M/len(X)
     repeatY = N/len(Y)
     for i in range (0, repeatX):
@@ -62,15 +46,31 @@ def isInterleaved(X, Y, S):
         if (len(S) == (i * len(X) + j * len(Y)) and opt[i*len(X)][j*len(Y)] == True):
           return True
     return False 
-  
-# A function to run test cases  
-def test(X, Y,S):  
-  
-    if (isInterleaved(X, Y, S)):  
-        print(S, "is interleaved of", X, "and", Y)  
-    else: 
-        print(S, "is not interleaved of", X, "and", Y) 
-  
-# Driver Code  
-if __name__ == '__main__':  
-    test("101", "0", "1000101011010")
+
+def test(arg):
+    f=open(str(arg), "r")
+    lines = f.read()
+    f.close()
+    global numComparisions
+    r=open("results/results.txt", "w+")
+    st = lines.split()
+    X = st[0]
+    Y = st[1]
+    S = st[2]
+    r.write("X string: " + X + " Y string: " + Y + " S string: " + S + "\n")
+    if (isInterleaved(X, Y, S)):
+        r.write(S+ " is interleaved of "+ X + " and "+ Y + "\n")
+        print(S, "is interleaved of", X, "and", Y)
+    else:
+        r.write(S + " is not interleaved of " +  X + " and " + Y + "\n")
+        print(S, "is not interleaved of", X, "and", Y)
+    r.write("Total number of comparisons is: "  + str(numComparisions) + "\n")
+    r.close() 
+parser = argparse.ArgumentParser()
+parser.add_argument("-il", help="Determines if string is an interleaving of two other strings", type=test, action="store")
+
+if len(sys.argv) <= 1:
+  sys.argv.append('--help')
+
+options = parser.parse_args()
+# if __name__ == '__m
